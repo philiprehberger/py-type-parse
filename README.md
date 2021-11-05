2c726fe845820d4ef7634dbc36c0ef632756aea6
Philip Rehberger <me@philiprehberger.com>
2021-11-04 20:58:24 -0500

    Add new features for v0.2.0

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![PyPI version](https://img.shields.io/pypi/v/philiprehberger-type-parse.svg)](https://pypi.org/project/philiprehberger-type-parse/)
 [![License](https://img.shields.io/github/license/philiprehberger/py-type-parse)](LICENSE)
 
-Parse strings into Python types intelligently — booleans, numbers, dates, None, lists.
+Parse strings into Python types intelligently — booleans, numbers, dates, datetimes, UUIDs, filesizes, None, lists.
 
 ## Install
 
@@ -55,24 +55,73 @@ parse_date("15.03.2026", dayfirst=True)  # datetime.date(2026, 3, 15)
 parse_date("March 15, 2026")          # datetime.date(2026, 3, 15)
 ```
 
+### Datetime Parsing
+
+```python
+from philiprehberger_type_parse import parse_datetime
+
+parse_datetime("2026-03-16 14:30:00")          # datetime(2026, 3, 16, 14, 30)
+parse_datetime("2026-03-16T14:30:00Z")         # datetime(2026, 3, 16, 14, 30, tzinfo=UTC)
+parse_datetime("2026-03-16T14:30:00+02:00")    # datetime with +02:00 offset
+parse_datetime("Mar 16, 2026 2:30 PM")         # datetime(2026, 3, 16, 14, 30)
+```
+
+### Time Parsing
+
+```python
+from philiprehberger_type_parse import parse_time
+
+parse_time("14:30")             # time(14, 30)
+parse_time("2:30 PM")           # time(14, 30)
+parse_time("14:30:00")          # time(14, 30)
+parse_time("14:30:00.123456")   # time(14, 30, 0, 123456)
+```
+
+### UUID Parsing
+
+```python
+from philiprehberger_type_parse import parse_uuid
+
+parse_uuid("550e8400-e29b-41d4-a716-446655440000")    # UUID('550e8400-...')
+parse_uuid("550e8400e29b41d4a716446655440000")         # UUID without hyphens
+parse_uuid("{550e8400-e29b-41d4-a716-446655440000}")   # UUID with braces
+```
+
+### Bytes / Filesize Parsing
+
+```python
+from philiprehberger_type_parse import parse_bytes
+
+parse_bytes("1.5 GB")     # 1500000000 (SI, base 1000)
+parse_bytes("1.5 GiB")    # 1610612736 (IEC, base 1024)
+parse_bytes("500 MB")     # 500000000
+parse_bytes("1024 KB")    # 1024000
+```
+
 ### List Parsing
 
 ```python
 from philiprehberger_type_parse import parse_list
 
-parse_list("a, b, c")           # ["a", "b", "c"]
-parse_list("x|y|z", separator="|")  # ["x", "y", "z"]
+parse_list("a, b, c")                        # ["a", "b", "c"]
+parse_list("x|y|z", separator="|")           # ["x", "y", "z"]
+parse_list("1, true, hello", coerce=True)    # [1, True, "hello"]
+parse_list("42, 3.14, none", coerce=True)    # [42, 3.14, None]
 ```
 
 ## API
 
 | Function | Description |
 |----------|-------------|
-| `parse(value)` | Auto-detect type: None, bool, int, float, date, or str |
+| `parse(value)` | Auto-detect type: None, bool, int, float, UUID, datetime, date, or str |
 | `parse_bool(value)` | Parse true/yes/1/on/y or false/no/0/off/n |
 | `parse_number(value)` | Parse int or float with commas, currency, percentages |
 | `parse_date(value, *, dayfirst=False)` | Parse common date formats into `datetime.date` |
-| `parse_list(value, *, separator=",")` | Split into trimmed, non-empty strings |
+| `parse_datetime(value, *, dayfirst=False)` | Parse date-time strings with optional timezone into `datetime.datetime` |
+| `parse_time(value)` | Parse time strings (24h, 12h AM/PM) into `datetime.time` |
+| `parse_uuid(value)` | Parse UUID strings (with/without hyphens/braces) into `uuid.UUID` |
+| `parse_bytes(value)` | Parse filesize strings (KB, MB, GB, KiB, MiB, GiB, etc.) to bytes |
+| `parse_list(value, *, separator=",", coerce=False)` | Split into trimmed, non-empty strings; optionally auto-detect types |
 
 
 ## Development
--- src/philiprehberger_type_parse/__init__.py
+++ src/philiprehberger_type_parse/__init__.py
@@ -1,11 +1,22 @@
-"""Parse strings into Python types intelligently — booleans, numbers, dates, None, lists."""
+"""Parse strings into Python types intelligently — booleans, numbers, dates, datetimes, UUIDs, filesizes, None, lists."""
 
 from __future__ import annotations
 
 import datetime
 import re
+import uuid as _uuid_mod
 
-__all__ = ["parse", "parse_bool", "parse_date", "parse_list", "parse_number"]
+__all__ = [
+    "parse",
+    "parse_bool",
+    "parse_bytes",
+    "parse_date",
+    "parse_datetime",
+    "parse_list",
+    "parse_number",
+    "parse_time",
+    "parse_uuid",
+]
 
 _TRUE_VALUES = frozenset({"true", "yes", "1", "on", "y"})
 _FALSE_VALUES = frozenset({"false", "no", "0", "off", "n"})
@@ -32,14 +43,79 @@ _DATE_FORMATS_DAYFIRST = (
     "%Y%m%d",
 )
 
+_DATETIME_FORMATS = (
+    "%Y-%m-%dT%H:%M:%S",
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%dT%H:%M",
+    "%Y-%m-%d %H:%M",
+    "%Y/%m/%d %H:%M:%S",
+    "%m/%d/%Y %H:%M:%S",
+    "%B %d, %Y %I:%M %p",
+    "%b %d, %Y %I:%M %p",
+    "%B %d, %Y %I:%M:%S %p",
+    "%b %d, %Y %I:%M:%S %p",
+)
+
+_DATETIME_FORMATS_DAYFIRST = (
+    "%Y-%m-%dT%H:%M:%S",
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%dT%H:%M",
+    "%Y-%m-%d %H:%M",
+    "%Y/%m/%d %H:%M:%S",
+    "%d/%m/%Y %H:%M:%S",
+    "%B %d, %Y %I:%M %p",
+    "%b %d, %Y %I:%M %p",
+    "%B %d, %Y %I:%M:%S %p",
+    "%b %d, %Y %I:%M:%S %p",
+)
+
+_TIME_FORMATS = (
+    "%H:%M:%S.%f",
+    "%H:%M:%S",
+    "%H:%M",
+    "%I:%M:%S %p",
+    "%I:%M %p",
+)
+
 _CURRENCY_RE = re.compile(r"^[£$€¥₹]?\s*-?\s*[\d,]+\.?\d*$")
 _PERCENT_RE = re.compile(r"^-?[\d,]+\.?\d*\s*%$")
 
+# Timezone offset pattern: +HH:MM, -HH:MM, +HHMM, -HHMM, Z
+_TZ_RE = re.compile(r"(Z|[+-]\d{2}:?\d{2})$")
 
-def parse(value: str) -> None | bool | int | float | datetime.date | str:
+# UUID pattern: 32 hex chars with optional hyphens and optional braces
+_UUID_RE = re.compile(
+    r"^\{?[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}\}?$",
+    re.IGNORECASE,
+)
+
+# Bytes/filesize units (SI and IEC)
+_BYTES_UNITS: dict[str, int] = {
+    "b": 1,
+    "kb": 1_000,
+    "mb": 1_000_000,
+    "gb": 1_000_000_000,
+    "tb": 1_000_000_000_000,
+    "pb": 1_000_000_000_000_000,
+    "kib": 1_024,
+    "mib": 1_048_576,
+    "gib": 1_073_741_824,
+    "tib": 1_099_511_627_776,
+    "pib": 1_125_899_906_842_624,
+}
+
+_BYTES_RE = re.compile(
+    r"^\s*(-?\d+(?:\.\d+)?)\s*(b|kb|mb|gb|tb|pb|kib|mib|gib|tib|pib)?\s*$",
+    re.IGNORECASE,
+)
+
+
+def parse(
+    value: str,
+) -> None | bool | int | float | _uuid_mod.UUID | datetime.datetime | datetime.date | str:
     """Auto-detect and parse a string into the most appropriate Python type.
 
-    Detection order: None -> bool -> int -> float -> date (YYYY-MM-DD) -> str.
+    Detection order: None -> bool -> int -> float -> uuid -> datetime -> date -> str.
     """
     stripped = value.strip()
 
@@ -53,6 +129,16 @@ def parse(value: str) -> None | bool | int | float | datetime.date | str:
 
     try:
         return parse_number(stripped)
+    except ValueError:
+        pass
+
+    try:
+        return parse_uuid(stripped)
+    except ValueError:
+        pass
+
+    try:
+        return parse_datetime(stripped)
     except ValueError:
         pass
 
@@ -139,15 +225,147 @@ def parse_date(value: str, *, dayfirst: bool = False) -> datetime.date:
     raise ValueError(f"Cannot parse {value!r} as date")
 
 
-def parse_list(value: str, *, separator: str = ",") -> list[str]:
+def parse_datetime(value: str, *, dayfirst: bool = False) -> datetime.datetime:
+    """Parse a string into a datetime object.
+
+    Supports ISO 8601 format, common date-time patterns, and timezone
+    offsets (``Z``, ``+02:00``, ``-0500``).
+
+    Raises:
+        ValueError: If the string cannot be parsed as a datetime.
+    """
+    stripped = value.strip()
+
+    # Try fromisoformat first (handles most ISO 8601 including tz)
+    # Python 3.11+ fromisoformat handles "Z" suffix; for 3.10 compatibility
+    # we replace trailing Z with +00:00.
+    iso_candidate = stripped
+    if iso_candidate.upper().endswith("Z"):
+        iso_candidate = iso_candidate[:-1] + "+00:00"
+    try:
+        return datetime.datetime.fromisoformat(iso_candidate)
+    except ValueError:
+        pass
+
+    # Strip timezone suffix for format-based parsing, then re-apply
+    tz_info: datetime.timezone | None = None
+    core = stripped
+    tz_match = _TZ_RE.search(stripped)
+    if tz_match:
+        tz_str = tz_match.group(1)
+        core = stripped[: tz_match.start()].rstrip()
+        if tz_str.upper() == "Z":
+            tz_info = datetime.timezone.utc
+        else:
+            sign = 1 if tz_str[0] == "+" else -1
+            digits = tz_str[1:].replace(":", "")
+            hours, minutes = int(digits[:2]), int(digits[2:4])
+            tz_info = datetime.timezone(
+                datetime.timedelta(hours=sign * hours, minutes=sign * minutes)
+            )
+
+    formats = _DATETIME_FORMATS_DAYFIRST if dayfirst else _DATETIME_FORMATS
+    for fmt in formats:
+        try:
+            dt = datetime.datetime.strptime(core, fmt)
+            if tz_info is not None:
+                dt = dt.replace(tzinfo=tz_info)
+            return dt
+        except ValueError:
+            continue
+
+    raise ValueError(f"Cannot parse {value!r} as datetime")
+
+
+def parse_time(value: str) -> datetime.time:
+    """Parse a string into a time object.
+
+    Supports 24-hour (``14:30``, ``14:30:00``), 12-hour (``2:30 PM``),
+    and microsecond precision (``14:30:00.123456``).
+
+    Raises:
+        ValueError: If the string cannot be parsed as a time.
+    """
+    stripped = value.strip()
+
+    for fmt in _TIME_FORMATS:
+        try:
+            return datetime.datetime.strptime(stripped, fmt).time()
+        except ValueError:
+            continue
+
+    raise ValueError(f"Cannot parse {value!r} as time")
+
+
+def parse_uuid(value: str) -> _uuid_mod.UUID:
+    """Parse a string into a UUID object.
+
+    Accepts standard UUID strings with or without hyphens, and optionally
+    wrapped in braces (``{...}``).
+
+    Raises:
+        ValueError: If the string is not a valid UUID.
+    """
+    stripped = value.strip()
+    if not _UUID_RE.match(stripped):
+        raise ValueError(f"Cannot parse {value!r} as UUID")
+    # Remove braces if present
+    cleaned = stripped.strip("{}")
+    return _uuid_mod.UUID(cleaned)
+
+
+def parse_bytes(value: str) -> int:
+    """Parse a filesize string into a number of bytes.
+
+    Supports SI units (KB, MB, GB, TB, PB — base 1000) and IEC units
+    (KiB, MiB, GiB, TiB, PiB — base 1024). Unit matching is
+    case-insensitive.
+
+    Examples:
+        >>> parse_bytes("1.5 GB")
+        1500000000
+        >>> parse_bytes("1.5 GiB")
+        1610612736
+        >>> parse_bytes("500 MB")
+        500000000
+
+    Raises:
+        ValueError: If the string cannot be parsed as a filesize.
+    """
+    stripped = value.strip()
+    match = _BYTES_RE.match(stripped)
+    if not match:
+        raise ValueError(f"Cannot parse {value!r} as bytes")
+
+    number_str, unit_str = match.group(1), match.group(2)
+    number = float(number_str)
+
+    if unit_str is None:
+        # Bare number, treat as bytes
+        multiplier = 1
+    else:
+        multiplier = _BYTES_UNITS.get(unit_str.lower(), 1)
+
+    return int(number * multiplier)
+
+
+def parse_list(
+    value: str, *, separator: str = ",", coerce: bool = False
+) -> list[str] | list[None | bool | int | float | _uuid_mod.UUID | datetime.datetime | datetime.date | str]:
     """Split a string into a list of stripped, non-empty strings.
 
     Args:
         value: The input string to split.
         separator: The delimiter to split on. Defaults to ``","``.
+        coerce: When ``True``, run :func:`parse` on each element to
+            auto-detect types. Defaults to ``False``.
 
     Returns:
-        A list of trimmed strings with empty entries removed.
+        A list of trimmed strings (or parsed values when *coerce* is True)
+        with empty entries removed.
     """
     parts = value.split(separator)
-    return [part.strip() for part in parts if part.strip()]
+    items = [part.strip() for part in parts if part.strip()]
+    if coerce:
+        return [parse(item) for item in items]
+    return items
